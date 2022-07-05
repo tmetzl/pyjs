1e954dc0494a3ff71b5c80090055c4b24778af64
Tim Metzler <tim.metzler@h-brs.de>
2022-07-05 23:20:41 +0200

    Create some js

---
+++ packages/header/src/index.js
@@ -0,0 +1,11 @@
+console.log("Hello there!");
+
+class Stuff {
+  constructor(alphabeta, gamma, delta, epsilon, and, so, on) {
+    this.alphabeta = alphabeta;
+  }
+
+  sayHi() {
+    console.log("HI");
+  }
+}
